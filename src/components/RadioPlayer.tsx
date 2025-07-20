e4afc4d32f397245b83ebbf2d6242afd1b8dd443
Edu <kaedu18@gmail.com>
2025-07-19 20:43:34 -0500

    agregando botones

--- src/components/RadioPlayer.tsx
+++ src/components/RadioPlayer.tsx
@@ -1,19 +1,34 @@
 import { useRef, useState } from "react"
 import { FetchApi } from "../utils/FetchApi"
 import MediaPLayer from '../assets/MediaPlayer.png'
+import { CaretRightOutlined, MutedOutlined, PauseOutlined, SoundOutlined } from "@ant-design/icons"
 
 const RadioPlayer = () => {
     const streamUrl = 'https://tupanel.info:8780/stream'
     const [playing, SetPlaying] = useState(false)
+    const [muted, SetMuted] = useState(false)
     const AudioRef = useRef<HTMLAudioElement>(null)
 
     const tigger = () => {
         if (!AudioRef.current) return
 
         playing ? AudioRef.current.pause() : AudioRef.current.play()
+
         SetPlaying(!playing)
+        SetMuted(!muted)
         FetchApi()
     }
+    const tiggerMuted = () => {
+        if (!AudioRef.current) return
+
+        playing ? AudioRef.current.muted = false : AudioRef.current.muted = true
+        // console.log(muted)
+        SetMuted(!muted)
+    }
+    const now = new Date()
+    const hour = now.getHours()
+    const minuts = now.getMinutes()
+
 
     /* https://de1.api.radio-browser.info/json/stations/bycountry/Peru */
 
@@ -25,12 +40,30 @@ const RadioPlayer = () => {
                     alt="Radiomar Logo"
                     className="w-full shadow-lg rounded-xl"
                 />
+                <div className="flex">
+                    <div>
+                        <h2 className="text-2xl font-bold uppercase">Viva FM</h2>
+                        <p>PE</p>
+                    </div>
+                    <div>
+                        <h2 className="font-light">tag</h2>
+                    </div>
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
 
                 <button
                     onClick={tigger}
-                    className="absolute bottom-96 right-4 bg-purple-600 text-white px-4 py-2 rounded-full text-sm hover:bg-purple-700 transition-all"
+                    className="absolute top-48 left-33  bg-transparent  text-zinc-500 text-4xl px-6 py-6 rounded-full  hover:bg-purple-700 transition-all"
                 >
-                    {playing ? "⏸️" : "▶️"}
+                    {playing ? <PauseOutlined /> : <CaretRightOutlined />}
+                </button>
+                <button
+                    onClick={tiggerMuted}
+                    className="absolute top-48 left-61  bg-transparent  text-zinc-500 text-4xl px-6 py-6 rounded-full"
+                >
+                    {muted ? <SoundOutlined /> : <MutedOutlined />}
                 </button>
             </div>
 
